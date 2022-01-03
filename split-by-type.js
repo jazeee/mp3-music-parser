47432acb403f515b8a589b195d9a233468c98fca
Jaz Singh <jaz@color.com>
2022-01-03 10:37:12 -0800

    Add Split by type

---
+++ split-by-type.js
@@ -0,0 +1,24 @@
+const { readFile, writeFile } = require('fs').promises;
+
+async function splitByType(sourceFileName) {
+  const data = JSON.parse(await readFile(sourceFileName));
+  const byArtist = {};
+  const byAlbum = {};
+  const byGenre = {};
+  const byYear = {};
+  const byDecade = {};
+  data.forEach((datum, index) => {
+    const { artist = 'Unknown', album = 'Unknown', genre = 'Unknown', year } = datum;
+    let decade = '-';
+    if (year) {
+      decade = year.replace(/.$/, '0');
+    } else {
+      year = '-';
+    }
+    byDecade[decade] = byDecade[decade]?.push(index) ?? [index];
+    byYear[year] = byYear[year]?.push(index) ?? [index];
+    byArtist[artist] = byArtist[artist]?.push(index) ?? [index];
+    byGenre[genre] = byGenre[genre]?.push(index) ?? [index];
+    byAlbum[album] = byAlbum[album]?.push(index) ?? [index];
+  })
+}
